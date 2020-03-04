36ce31c0e00e86fd03df3cb10fb4c366327951ad
Serenity911 <silvia.simonassi01@gmail.com>
2020-03-04 10:44:13 +0000

    created structure and cell

---
+++ src/components/CellItem.js
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from 'react';
+
+class CellItem extends Component{
+
+    render() {
+        return(
+            <p>I am a cell item</p>
+        )
+    }
+}
+
+export default CellItem
---
+++ src/containers/GameBox.js
@@ -0,0 +1,34 @@
+import React, { Component, Fragment } from 'react';
+import CellItem from '../components/CellItem.js'
+
+class GameBox extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            winner: '',
+            player1Moved: false,
+            player2Moved: true,
+            cells: [
+                {R1C1: '', isPlayable: true}, {R1C2: '', isPlayable: true}, {R1C3: '', isPlayable: true}
+            ]
+        }
+    }
+
+    render(){
+        const createCells = this.state.cells.map(cell => {
+            const cellId = Object.keys(cell)[0]
+            const cellValue = Object.values(cell)[0]
+            return <CellItem key={cellId}>{cellValue}</CellItem>
+        })
+
+        return(
+            <Fragment>
+            <p>HELLO I AM THE BOX</p>
+            {createCells}
+            </Fragment>
+        )
+    }
+
+}
+
+export default GameBox
