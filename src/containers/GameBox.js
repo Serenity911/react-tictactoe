53085744a61bf54b2937cacec270b034d1c1de09
Chris Fraser <chrisssfraser@gmail.com>
2020-03-04 15:19:32 +0000

    create rows and loops for winning conditions

--- src/containers/GameBox.js
+++ src/containers/GameBox.js
@@ -8,13 +8,58 @@ class GameBox extends Component{
             winner: '',
             players: [ {id: "player1", symbol: "X", isCurrentPlayer: true }, {id: "player2", symbol: "O", isCurrentPlayer: false }],            
             cells: [
-                {id: 'R1C1', value: '', isPlayable: true}, {id: 'R1C2', value: '', isPlayable: true}, {id: 'R1C3', value: '', isPlayable: true}
+                {id: 'R1C1', value: '', isPlayable: true}, {id: 'R1C2', value: '', isPlayable: true}, {id: 'R1C3', value: '', isPlayable: true}, {id: 'R2C1', value: '', isPlayable: true}, {id: 'R2C2', value: '', isPlayable: true}, {id: 'R2C3', value: '', isPlayable: true}, {id: 'R3C1', value: '', isPlayable: true}, {id: 'R3C2', value: '', isPlayable: true}, {id: 'R3C3', value: '', isPlayable: true}
             ]
         }
         this.handleCellClick = this.handleCellClick.bind(this)
         this.whoIsCurrentPlayer = this.whoIsCurrentPlayer.bind(this)
+        this.getFirstRowValues = this.getFirstRowValues.bind(this)
+        this.getFirstRowWinner = this.getFirstRowWinner.bind(this)
+        // this.getEachRow = this.getEachRow.bind(this)
+        this.getRowsWinner = this.getRowsWinner.bind(this)
     }
 
+    getArrayOfRows(){
+        const arrayOfRows = []
+        for(let i = 0; i <=8; i +=3 ){
+            let arrayRow = [this.state.cells[i].value, this.state.cells[i+1].value, this.state.cells[i+2].value]
+            arrayOfRows.push(arrayRow)
+        }
+        return arrayOfRows 
+    }
+
+    getRowsWinner(){
+        for (const row of this.getArrayOfRows()) {
+            console.log(row);
+            
+            if (row.includes("X") && !row.includes("O") && !row.includes("")){
+                console.log("Player1 won");   
+            }
+            if(row.includes("O") && !row.includes("X") && !row.includes("")){
+                console.log("Player2 won");   
+            }
+        }
+
+    }
+
+    getFirstRowValues(){
+        const firstRowValue = this.state.cells.map( (item, index) => {
+            if(index <=2){
+                return item.value
+            }
+        })
+        return firstRowValue
+    }
+
+    getFirstRowWinner(){
+        if (this.getFirstRowValues().includes("X") && !this.getFirstRowValues().includes("O") && !this.getFirstRowValues().includes("")){
+            console.log("Player1 won");   
+        }
+        if(this.getFirstRowValues().includes("O") && !this.getFirstRowValues().includes("X") && !this.getFirstRowValues().includes("")){
+            console.log("Player2 won");   
+
+        }
+    }
 
     handleNextTurn(){
         const currentPlayer = this.whoIsCurrentPlayer()
@@ -57,8 +102,9 @@ class GameBox extends Component{
 
         return(
             <Fragment>
-    <h2> Which player is playing? {this.whoIsCurrentPlayer().id}</h2>
+            <h2> Which player is playing? {this.whoIsCurrentPlayer().id}</h2>
             <p>HELLO I AM THE BOX</p>
+            <button onClick={this.getRowsWinner}></button>
             {createCells}
             </Fragment>
         )
